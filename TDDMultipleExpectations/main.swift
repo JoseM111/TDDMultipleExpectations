67a1ac95c4b4995b1ed7f937c55e2b47a581c85f
[jose-martinez] <alias_one11@outlook.com>
2020-06-04 21:30:52 +0200

    Initial Commit

---
+++ TDDMultipleExpectations/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TDDMultipleExpectations
+//
+//  Created by Jose Martinez on 6/4/20.
+//  Copyright © 2020 Jose Martinez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
